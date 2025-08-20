f05ff77ac9be7ebbd1e0301843990e52805f88bc
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-08-20 12:18:29 +0530

    updated v1.js

--- routes/v1.js
+++ routes/v1.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 // Example Route Imports
 // const exampleRouter = require("./example.route");
+const userRouter = require("./user.route")
+const genderRouter = require("./gender.route")
+const usertypeRouter = require("./usertype.route")
 
 // Health Check Route
 router.get("/health", (req, res) => {
@@ -13,7 +16,8 @@ router.get("/health", (req, res) => {
 
 // Add your route imports and `router.use()` registrations below
 // router.use("/example", exampleRouter);
-
-
+router.use("/users", userRouter);
+router.use("/genders", genderRouter);
+router.use("/usertypes", usertypeRouter);
 
 module.exports = router;
