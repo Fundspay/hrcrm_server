fb9c1fbe3557554bc56891a4e46737dd8db0d284
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-08-20 11:17:54 +0530

    updated v1.js files

--- routes/v1.js
+++ routes/v1.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../middleware/auth.middleware");
+
 
 // Example Route Imports
 // const exampleRouter = require("./example.route");
