09a725fac04728cb511a4f1414ceb3932393c2c9
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-08-20 12:15:34 +0530

    updated user model

---
+++ models/user.model.js
@@ -0,0 +1,38 @@
+"use strict";
+module.exports = (sequelize, Sequelize) => {
+    const User = sequelize.define(
+        "User",
+        {
+            id: { autoIncrement: true, primaryKey: true, type: Sequelize.BIGINT },
+            firstName: { type: Sequelize.STRING, allowNull: false },
+            lastName: { type: Sequelize.STRING, allowNull: false },
+            gender: { type: Sequelize.BIGINT, allowNull: true },
+            email: { type: Sequelize.STRING, allowNull: true, unique: true },
+            phoneNumber: { type: Sequelize.STRING, allowNull: true, unique: true },
+            photoUrl: { type: Sequelize.STRING, allowNull: true },
+            type: { type: Sequelize.BIGINT, allowNull: true }, // user type
+            isActive: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: true },
+            createdAt: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.NOW },
+            updatedAt: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.NOW },
+        },
+        { timestamps: true }
+    );
+
+    User.associate = function (models) {
+        User.belongsTo(models.UserType, {
+            foreignKey: 'type',
+            onDelete: 'RESTRICT',
+            onUpdate: 'RESTRICT',
+            constraints: true,
+        });
+
+        User.belongsTo(models.Gender, {
+            foreignKey: 'gender',
+            onDelete: 'RESTRICT',
+            onUpdate: 'RESTRICT',
+            constraints: true,
+        });
+    };
+
+    return User;
+};
