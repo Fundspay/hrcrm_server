b36b492b0eb200805581ac16259a2e5fd75d28cb
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-08-20 13:53:51 +0530

    updated with mailer envs

--- config/config.js
+++ config/config.js
@@ -12,10 +12,23 @@ CONFIG.port = process.env.PORT || 3000;
 CONFIG.db_dialect   = process.env.DB_DIALECT || "postgres";
 CONFIG.db_host      = process.env.DB_HOST || "localhost";
 CONFIG.db_port      = process.env.DB_PORT || 5432;
-CONFIG.db_name      = process.env.DB_NAME || "hr_crm";
+CONFIG.db_name      = process.env.DB_NAME || "hr";
 CONFIG.db_user      = process.env.DB_USER || "postgres";
 CONFIG.db_password  = process.env.DB_PASSWORD || "Root";
 CONFIG.db_usePassword = (process.env.DB_USE_PASSWORD || "true") === "true";
+
+
+CONFIG.godaddyBaseUrl = process.env.GODADDY_BASE_URL;
+CONFIG.godaddyApiKey = process.env.GODADDY_API_KEY;
+CONFIG.godaddyApiSecret = process.env.GODADDY_API_SECRET;
+
+// Mail (GoDaddy SMTP)
+CONFIG.mailService = process.env.MAIL_SERVICE;
+CONFIG.mailUser = process.env.MAIL_USER;
+CONFIG.mailPassword = process.env.MAIL_PASSWORD;
+CONFIG.mailHost = process.env.MAIL_HOST;
+CONFIG.mailPort = process.env.MAIL_PORT;
+CONFIG.mailSecure = process.env.MAIL_SECURE;
 
 
 module.exports = CONFIG;  // <-- ✅ export it
