b5a8dd3cfe3e10464e944dbc9cb8607bf1d8f0bf
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-08-20 12:14:57 +0530

    created user route

---
+++ routes/user.route.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/user.controller");
+
+// ✅ User CRUD
+router.post("/register", userController.addUser);         // Create user
+router.get("/list", userController.fetchAllUsers);       // Get all users
+router.get("/list/:id", userController.fetchSingleUser); // Get single user
+router.put("/update/:id", userController.updateUser);    // Update user
+router.delete("/delete/:id", userController.deleteUser); // Soft delete user
+
+// ✅ Auth
+router.post("/login", userController.loginWithEmailPassword); // Login with email & password
+
+module.exports = router;
