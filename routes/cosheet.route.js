24052735c11a8448e92ca11e0fd13af5da0fe510
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-08-21 15:35:37 +0530

    updated co sheet route

--- routes/cosheet.route.js
+++ routes/cosheet.route.js
@@ -8,5 +8,12 @@ router.post("/register", cosheetController.createCoSheet);
 router.put("/update/:id", cosheetController.updateConnectFields);
 router.get("/list", cosheetController.getCoSheets);
 router.get("/list/:id", cosheetController.getCoSheetById);
+router.post("/:id/send-jd", cosheetController.sendJDToCollege);
+router.get("/stats/all", cosheetController.getCallStatsAllUsers);
+router.get("/stats/user/:userId", cosheetController.getCallStatsByUser);
+router.get("/stats/user/:userId/jd", cosheetController.getJdStats);
+// router.get("/stats/user/:userId?fromDate=YYYY-MM-DD&toDate=YYYY-MM-DD", cosheetController.getJdStats);
+router.get("/internship-stats/:userId", cosheetController.getInternshipStats);
+router.get("/internship-type-colleges", cosheetController.getInternshipTypeColleges);
 
 module.exports = router;
