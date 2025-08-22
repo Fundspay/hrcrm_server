c7fc9515e375968003ea6562234e927a45cea4a7
Atif-pixel <atifmulla9695@gmail.com>
2025-08-22 11:07:29 +0530

    added allow origin cors

--- server.js
+++ server.js
@@ -24,10 +24,7 @@ app.use(compression());
 
 // ────── CORS ───────────────────────────────────────────
 const corsOptions = {
-  origin: [
-    "http://localhost:8080",
-    "https://fundsaudit.com"
-  ],
+  origin: "*",
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "Accept"],
   credentials: true,
