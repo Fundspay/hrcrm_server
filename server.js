d8da1b6283e4fac95ce21b9e5dd9446738b913df
Atif-pixel <atifmulla9695@gmail.com>
2025-08-26 00:32:03 +0530

    solved logout issue

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(compression());
 const corsOptions = {
   origin: "*",
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization", 'Accept'],
   credentials: true,
   optionsSuccessStatus: 204
 };
