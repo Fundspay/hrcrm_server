0359fa210dc1896d6f490cabf424de49248a0510
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-09-01 13:03:29 +0530

    updated resume details controller #wip15

--- controllers/resumedetails.controller.js
+++ controllers/resumedetails.controller.js
@@ -592,7 +592,7 @@ const getAllPendingFollowUps = async (req, res) => {
       success: true,
       totalRecords: coSheetData.length,
       data: coSheetData,
-      usersList: users, // 👈 always include user list
+      users, // 👈 always include user list
     });
   } catch (error) {
     console.error("Get All Pending FollowUps Error:", error);
