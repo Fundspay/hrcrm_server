a6b1454cbaa50ac28c4eb4511c73c762e72ba714
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-09-03 12:23:41 +0530

    I sheet end points modification #wip23

--- routes/interview.route.js
+++ routes/interview.route.js
@@ -12,5 +12,12 @@ router.get("/interviewsummary/:userId", interviewController.getInterviewSummary)
 router.get("/collegeanalysis/:userId", interviewController.getCollegeInterviewAnalysis);
 router.get("/interviews/:userId", interviewController.listInterviewsByUserId);
 router.get("/dailyinterviewstats/:userId", interviewController.getDailyInterviewStats);
+router.get("/interviews/:userId/conducted", interviewController.listConductedInterviews);
+router.get("/interviews/:userId/selected", interviewController.listSelectedInterviews);
+router.get("/interviews/:userId/onhold", interviewController.listOnHoldInterviews);
+router.get("/interviews/:userId/notanswered", interviewController.listNotAnsweredInterviews);
+router.get("/interviews/:userId/notselected", interviewController.listNotSelectedInterviews);
+router.get("/interviews/:userId/notinterested", interviewController.listNotInterestedInterviews);
+
 
 module.exports = router;
